44ffbaf959d030ee7fe02a88f3bf21b60f95dc19
Sethu <sethuserge@gmail.com>
2024-09-11 14:17:00 +0200

    Initial commit

---
+++ src/test/java/com/blogcms/backend/BlogCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogcms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
