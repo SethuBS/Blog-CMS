15d819e75b4b4e209df8ef17e6f0795d36362e54
Sethu <sethuserge@gmail.com>
2024-09-11 16:10:44 +0200

    Updated and added exception

--- src/main/java/com/blogcms/backend/exception/InvalidEnumValueException.java
+++ src/main/java/com/blogcms/backend/exception/InvalidEnumValueException.java
@@ -1,6 +1,7 @@
 package com.blogcms.backend.exception;
 
 public class InvalidEnumValueException extends RuntimeException {
+
     public InvalidEnumValueException(String message) {
         super(message);
     }
