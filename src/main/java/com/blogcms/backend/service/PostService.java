737a06b9641badcf8673ba9685b1b6a2dc1dcd8f
Sethu <sethuserge@gmail.com>
2024-09-11 16:08:12 +0200

    Repo and Service Added missed method

--- src/main/java/com/blogcms/backend/service/PostService.java
+++ src/main/java/com/blogcms/backend/service/PostService.java
@@ -5,7 +5,9 @@ import com.blogcms.backend.dto.PostDTO;
 import java.util.List;
 
 public interface PostService {
-    PostDTO createPost(PostDTO postDTO);
+    PostDTO createPost(PostDTO newPostDTO);
+
+    PostDTO updatePost(PostDTO updatedPostDTO, Long postId);
 
     List<PostDTO> getAllPosts();
 
